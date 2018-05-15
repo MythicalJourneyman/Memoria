4cc8a95d9b73f6a83394c5d1d105e552a4b8ac0d
prabodh <cs1080182@gmail.com>
2018-05-15 17:29:06 +0530

    game logic , icons , splash , leader board

---
+++ app/src/main/java/com/mythicaljourneyman/memoria/db/dao/LeaderboardDao.java
@@ -0,0 +1,23 @@
+package com.mythicaljourneyman.memoria.db.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.mythicaljourneyman.memoria.db.objects.LeaderboardItem;
+
+import java.util.List;
+
+/**
+ * Created by Prabodh Dhabaria on 14-05-2018.
+ */
+@Dao
+public interface LeaderboardDao {
+
+    @Query("SELECT * FROM leaderboard order by mScore desc limit :cLimit offset :cSkip")
+    List<LeaderboardItem> getSortedDataAll(int cLimit, int cSkip);
+
+    @Insert
+    void insertAll(LeaderboardItem... leaderboardItems);
+
+}
